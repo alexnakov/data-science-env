2162ce20508070c3f6f6d30ac7311a7dccd65207
alexnakov <alex.nakov3@gmail.com>
2025-08-02 15:21:38 +0100

    live video blurring

--- main.py
+++ main.py
@@ -3,29 +3,54 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import os
 from utils import *
-from PIL import Image
+import mediapipe as mp
+import argparse
 
-YELLOW = [0,255,255]
-video_capture = cv.VideoCapture(0)
-yellow_lower_limit, yellow_upper_limit = get_color_limits(YELLOW, tol=15)
+args = argparse.ArgumentParser()
+args.add_argument("--mode", default='video')
+args.add_argument("--filePath", default='./assets/person.jpg')
+parsed_args = args.parse_args()
 
-while True:
-  ret, frame = video_capture.read()
 
-  hsv_img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-  mask = cv.inRange(hsv_img, yellow_lower_limit, yellow_upper_limit)
-  mask_ = Image.fromarray(mask)
+def process_img(img, face_detection):
+  img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+  out = face_detection.process(img)
+  H, W, _ = img.shape
 
-  bbox = mask_.getbbox()
+  if out.detections is not None:
+    for detection in out.detections:
+      location_data = detection.location_data
+      bbox = location_data.relative_bounding_box
 
-  if bbox is not None:
-    x1,y1,x2,y2 = bbox
-    frame = cv.rectangle(frame, (x1,y1),(x2,y2),[0,255,0],2)
+      x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
 
-  cv.imshow('frame', frame)
+      x1 = int(x1 * W)
+      y1 = int(y1 * H)
+      w = int(w * W)
+      h = int(h * H)
 
-  if cv.waitKey(1) & 0xFF == ord('q'):
-    break
+      # cv.rectangle(img, (x1,y1),(x1+w,y1+h),[0,255,255],5)
+      img[y1:y1+h,x1:x1+w,:] = cv.blur(img[y1:y1+h,x1:x1+w,:], (27,27))
+  img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+  return img
 
-video_capture.release()
-cv.destroyAllWindows()
+img_path = os.path.join('./assets/','person.jpg')
+img = cv.imread(img_path)
+
+# detect faces
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+  if parsed_args.mode in ['image']:
+    img = cv.imread(parsed_args.filePath)
+    output_img = process_img(img, face_detection)
+    cv.imwrite(os.path.join('./assets/','outputPerson.jpg'), output_img)
+  elif parsed_args.mode in ['video']:
+    video_capture = cv.VideoCapture(0)
+    while True:
+      ret, frame = video_capture.read()
+      processed_frame = process_img(frame, face_detection)
+      cv.imshow('img', processed_frame)
+      if cv.waitKey(40) & 0xFF == ord('q'):
+        break
+
